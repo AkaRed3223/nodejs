3422ba0c0a1618298c49be7f99b3c71e13a139b8
AkaRed3223 <cesarsales22@gmail.com>
2022-12-14 19:34:16 -0300

    aa

--- login.js
+++ login.js
@@ -1,15 +1,51 @@
 const express = require('express')
 const router = express.Router()
+
+module.exports = () => {
+  const signUpRouter = new SignUpRouter()
+  router.post('/signup', ExpressRouterAdapter.adapt(signUpRouter))
+}
+
+class ExpressRouterAdapter {
+  static adapt (signUpRouter) {
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body
+      }
+      const httpResponse = await signUpRouter.route(httpRequest)
+      res.status(httpResponse.statusCode).body(httpResponse.body)
+    }
+  }
+}
+
+// signup-router
+class SignUpRouter {
+  async route (httpRequest) {
+    const { email, password, repeatPassword } = httpRequest.body
+    const user = new SignUpUseCase().signUp(email, password, repeatPassword)
+    return {
+      statusCode: 200,
+      body: user
+    }
+  }
+}
+
+// signup-usecase
+class SignUpUseCase {
+  async signUp (email, password, repeatPassword) {
+    if (password === repeatPassword) {
+      new AddAccountRepository().add(email, password)
+    }
+  }
+}
+
+// add-account-repo
 const mongoose = require('mongoose')
 const AccountModel = mongoose.model('Account')
 
-module.exports = () => {
-  router.post('/signup', async (req, res) => {
-    const { email, password, repeatPassword } = req.body
-    if (password === repeatPassword) {
-      const user = await AccountModel.create({ email, password })
-      return res.json(user)
-    }
-    res.status(400).json({ error: 'password must be equal to repeatPassword' })
-  })
+class AddAccountRepository {
+  async add (email, password) {
+    const user = await AccountModel.create({ email, password })
+    return user
+  }
 }
